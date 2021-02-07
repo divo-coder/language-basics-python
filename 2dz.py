072752d862232b7a92f536e555f4d45b67e40bd7
dim <di3970@ya.ru>
2021-02-07 19:38:49 +0300

    задание 2

--- 2dz.py
+++ 2dz.py
@@ -1,10 +1,4 @@
 
-my_list = [i for i in input('Введите список: ').split()]
-for i in range(1, len(my_list), 2):
-    my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
-print(' '.join([str(i) for i in my_list]))
-"""
-"""
 seasons_list = ["зима", "весна", "лето", "осень"]
 seasons_dict = {1: "зима", 2: "весна", 3: "лето", 4: "осень"}
 month = int(input("Введите число месяца "))
