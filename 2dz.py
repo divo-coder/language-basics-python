f3f3441be815f0b3bcdce290cd8f37e6ed77a947
dim <di3970@ya.ru>
2021-02-07 19:42:43 +0300

    задание 3

--- 2dz.py
+++ 2dz.py
@@ -1,25 +1,3 @@
-
-seasons_list = ["зима", "весна", "лето", "осень"]
-seasons_dict = {1: "зима", 2: "весна", 3: "лето", 4: "осень"}
-month = int(input("Введите число месяца "))
-if month == 1 or month == 12 or month == 2:
-        print(seasons_dict.get(1), " - список")
-        print(seasons_list[0], " - словарь")
-elif month == 3 or month == 4 or month == 5:
-    print(seasons_dict.get(2), " - список")
-    print(seasons_list[1], " - словарь")
-elif month == 6 or month == 7 or month == 8:
-    print(seasons_dict.get(3), " - список")
-    print(seasons_list[2], " - словарь")
-
-elif month == 9 or month == 10 or month == 11:
-    print(seasons_dict.get(4), " - список")
-    print(seasons_list[3], " - словарь")
-else:
-    print("Необходимо ввести число от 1 до 12")
-
-"""
-"""
 str1 = input("Введите строку: ")
 word = []
 num = 1
