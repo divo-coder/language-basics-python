21a5989d2ca6f9cd3110c4905bb3b5f819b16582
dim <di3970@ya.ru>
2021-02-07 19:47:22 +0300

    задания 4+5

--- 2dz.py
+++ 2dz.py
@@ -1,15 +1,3 @@
-str1 = input("Введите строку: ")
-word = []
-num = 1
-for i in range(str1.count(' ') + 1):
-    word = str1.split()
-    if len(str(word)) <= 10:
-        print(num, word[i])
-        num += 1
-    else:
-        print(num, word[i][0:10])
-        num += 1
-"""
 my_list = [7, 5, 3, 3, 2]
 print(my_list)
 num = int(input("Введите число: "))
