29127dd64e9d2dacb95f902c306264eeece16a8e
dim <di3970@ya.ru>
2021-02-07 19:33:10 +0300

    задание 1

---
+++ 1.1.py
@@ -0,0 +1,5 @@
+result_list = [0, 'Привет', 100, 100.1, None]
+i = 0
+while i < len(result_list):
+    print(type(result_list[i]))
+    i = i + 1
--- 2dz.py
+++ 2dz.py
@@ -1,11 +1,4 @@
-"""
-result_list = [0, 'Привет', 100, 100.1, None]
-i = 0
-while i < len(result_list):
-    print(type(result_list[i]))
-    i = i + 1
-"""
-""" 
+
 my_list = [i for i in input('Введите список: ').split()]
 for i in range(1, len(my_list), 2):
     my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
