b764da760474fe27f66ab5ff5c74fc9954554f98
dim <77855964+divo-coder@users.noreply.github.com>
2021-02-13 17:59:52 +0300

    Update 1.6.py

--- 1.6.py
+++ 1.6.py
@@ -7,3 +7,20 @@ while distance < target:
     i = i + 1
 print(i, "день :", round(distance, 2), "км")
 print("Вы достигнете желаемый результат на", i, "день.")
+"""
+# допиленное решение
+while True:
+    distance = int(input('Введите результат пробежки в первый день, км: '))
+    target = int(input('Введите значение цели, км: '))
+    i = 1
+    if target < distance:
+        print ("введены некорректные данные")
+    else:
+        while distance < target:
+            print(f"{i} день {distance:.2f} км")
+            distance = distance * 1.1
+            i = i + 1
+        print(f"{i} день {distance:.2f} км")
+        print(f"Вы достигнете желаемый результат на {i} день.")
+        break
+"""
